a9a7a8d21a4c0868addd481ec04fa6eb9a5fc379
zoldiklord <hamzahajjou22@gmail.com>
2022-06-19 02:09:12 +0000

    first commit

---
+++ screens/Login.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import {Fragment}from 'react'
+import { StyleSheet, SafeAreaView, View } from 'react-native'
+import { Button } from 'react-native-elements'
+import FormInput from '../components/FormInput'
+import FormButton from '../components/FormButton'
+import { Formik } from 'formik'
+
+export default class Login extends React.Component {
+    goToSignup = () => this.props.navigation.navigate('Signup')
+    render() {
+      return (
+        <SafeAreaView style={styles.container}>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            onSubmit={values => { alert(JSON.stringify(values))}}
+          >
+           {({ handleChange, values, handleSubmit }) => (
+    <Fragment>
+      <FormInput
+        name='email'
+        value={values.email}
+        onChangeText={handleChange('email')}
+        placeholder='Enter email'
+        autoCapitalize='none'
+        iconName='ios-mail'
+        iconColor='#2C384A'
+      />
+      <FormInput
+        name='password'
+        value={values.password}
+        onChangeText={handleChange('password')}
+        placeholder='Enter password'
+        secureTextEntry
+        iconName='ios-lock'
+        iconColor='#2C384A'
+      />
+      <View style={styles.buttonContainer}>
+        <FormButton
+          buttonType='outline'
+          onPress={handleSubmit}
+          title='LOGIN'
+          buttonColor='#039BE5'
+        />
+      </View>
+    </Fragment>
+  )
+}
+          </Formik>
+          <Button
+            title="Don't have an account? Sign Up"
+            onPress={this.goToSignup}
+            titleStyle={{
+              color: '#F57C00'
+            }}
+            type="clear"
+          />
+        </SafeAreaView>
+      )
+    }
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  buttonContainer: {
+    margin: 25
+  }
+})
